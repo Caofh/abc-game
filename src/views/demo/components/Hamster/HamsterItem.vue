f007a8b2996acc13968b4e60b1ee895db331d2d4
f45_100006550405_lijianjian <f45_100006550405_lijianjian@git.cloud.tencent.com>
2018-09-13 17:43:58 +0800

    游戏逻辑修改

--- src/views/demo/components/Hamster/HamsterItem.vue
+++ src/views/demo/components/Hamster/HamsterItem.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="box">
     <header class="header">
-      <div class="go">
-        <button class="begin" @click="begin">GO</button>
-      </div>
-      <div class="score">
-        <div>倒计时：{{time}}</div>
-        <div>得分：{{score}}</div>
-      </div>
+      <div class="time">倒计时：{{countDown}}</div>
+      <div class="score">得分：{{score}}</div>
     </header>
     <div class="wordImg">
       <img v-show="wordImg" style="width:100px;height:100px;vertical-align: middle" :src='wordImg' alt="单词对应的图片">
@@ -15,15 +10,26 @@
     <div class="content">
       <div class="hole-box">
         <div>
-          <div @click="choseWord(index,word)" v-for="(word,index) in words" class="hole" >
+          <div @click="choseWord(index,letter.letter)" v-for="(letter,index) in letters" class="hole">
             <div class="hamster" ref="hamster">
-              <span class="word">{{word}}</span>
+              <span class="word">{{letter.letter}}</span>
             </div>
             <div class="slice-hole"></div>
           </div>
         </div>
       </div>
 
+    </div>
+    <div class="footer">
+      <div class="go">
+        <button class="begin" @click="begin">GO</button>
+      </div>
+      <div class="next">
+        <button @click="nextWord">next</button>
+      </div>
+    </div>
+    <div class="congratulate" v-show="good">
+      <div>good</div>
     </div>
   </div>
 </template>
@@ -36,28 +42,34 @@
     height: 100%;
     background: url('../../../../assets/img/hamster/grassland.png');
   }
-  .header{
+
+  .header {
     overflow: hidden;
-    padding:10px;
-    .go{
+    padding: 10px;
+    .score, .time {
+      float: right;
+      color: #ffc107;
+      font-weight: 600;
+      font-size: 16px;
+    }
+    .time {
       float: left;
     }
-    .score{
-      float:right;
-      color:#ffc107;
-      font-weight: 600;
-    }
   }
-  .begin{
-    width:60px;
-    height:40px;
-    border-radius:10px;
-    background:#ffc107;
-    color:#fff;
-    font-size: 18px;
-    outline: none;
-    font-weight: 600;
+
+  .congratulate {
+    position: absolute;
+    z-index: 100;
+    width: 100px;
+    height: 60px;
+    background: #ffc107;
+    opacity: 0.8;
+    top: 30%;
+    left: 50%;
+    margin-left: -50px;
+    color: #fff;
   }
+
   .content {
     position: relative;
     flex-grow: 1;
@@ -79,66 +91,91 @@
     background: url('../../../../assets/img/hamster/hole.png');
     background-size: 100% 100%;
     margin-left: 20px;
-    margin-bottom: 40px;
+    margin-bottom: 20px;
     color: #fff;
     position: relative;
     //overflow: hidden;
-    .hamster{
-      width:90px;
-      height:80px;
-      margin:0 auto;
-      background:red;
+    .hamster {
+      width: 90px;
+      height: 80px;
+      margin: 0 auto;
+      background: red;
       display: none;
       background: url('../../../../assets/img/hamster/hamster_0.png') no-repeat;
       background-size: 100% 100%;
       position: absolute;
-      top:-35px;
+      top: -35px;
       /*transition:top 0.2s;*/
       /*-moz-transition:top 0.2s; !* Firefox 4 *!*/
       /*-webkit-transition:top 0.2s; !* Safari and Chrome *!*/
       /*-o-transition:top 0.2s; !* Opera *!*/
-      .word{
+      .word {
         position: absolute;
-        width:20px;
-        height:20px;
-        background:#fff;
+        width: 20px;
+        height: 20px;
+        background: #fff;
         border-radius: 50%;
-        top:-20px;
-        color:#ffa61f;
+        top: -20px;
+        color: #ffa61f;
       }
     }
-    .slice-hole{
-      width:100px;
-      height:20px;
-      background:url('../../../../assets/img/hamster/slice-holes.png') no-repeat;
+    .slice-hole {
+      width: 100px;
+      height: 20px;
+      background: url('../../../../assets/img/hamster/slice-holes.png') no-repeat;
       background-size: 100% 100%;
       position: absolute;
-      bottom:0;
+      bottom: 0;
     }
 
   }
+
+  .footer {
+    overflow: hidden;
+    padding: 20px;
+    button {
+      width: 60px;
+      height: 40px;
+      border-radius: 10px;
+      background: #ffc107;
+      color: #fff;
+      font-size: 18px;
+      outline: none;
+      font-weight: 600;
+      border: none;
+      outline: none;
+    }
+    .go {
+      float: left;
+    }
+    .next {
+      float: right;
+    }
+  }
 </style>
 <script>
-  import { MessageBox } from 'mint-ui';
+  import {MessageBox} from 'mint-ui';
   //获取随机数
   let random = (n, m) => {
     return Math.random() * (m - n) + n;
   }
   import allWords from './words.json'
 
-  let thisGameWords = [], randomIndex;
-  //获取本轮游戏的10个单词
-  for (let n = 0; n < 10; n++) {
-    randomIndex = Math.floor(random(0,allWords.length-1))
-    thisGameWords.push(allWords[randomIndex])
-  }
+  let wordLength = allWords.length;
+  let trueWord = '';
+  // let thisGameWords = [], randomIndex;
+  // //获取本轮游戏的10个单词
+  // for (let n = 0; n < 10; n++) {
+  //   randomIndex = Math.floor(random(0, allWords.length - 1))
+  //   thisGameWords.push(allWords[randomIndex])
+  // }
 
   let initWords = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
   let selectWord = ''
 
-  let num=0
-
+  let num = 0
+  let giveLetterTimer,hideHamsterTime
   export default {
     name: "HamsterItem",
     data() {
@@ -146,117 +183,106 @@
         //单词对应的图片
         wordImg: '',
         //洞中对应的单词队列
-        words: ['', '', '', '', '', '', '', '', ''],
+        letters: [{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0},{letter:'',showTime:0}],
         //倒计时
-        time: 20,
+        time: 180000,
         //得分
-        score:0,
-        holeNum:9
+        score: 0,
+        holeNum: 9,
+        good: false,
+      }
+    },
+    computed: {
+      countDown() {
+        let m = Math.floor(this.time / 1000 / 60),
+          s = (this.time - m * 60 * 1000) / 1000;
+        return `${m}′${s}″`
       }
     },
     mounted() {
 
     },
+    beforeDestroy() {
+      clearTimeout(giveLetterTimer)
+    },
+
     methods: {
       //根据单词获取对应的图片
-      getImg(word){
+      getImg(word) {
         this.wordImg = require(`../../../../assets/img/hamster/${word}.jpeg`)
       },
-      gameOver(){
+      gameOver() {
         MessageBox({
           title: 'game over',
           message: `恭喜，你的得分是 ${this.score}，你可以邀请你的小伙伴一起玩，或者查看你的排名哦`,
           showCancelButton: true,
-          confirmButtonText:'邀请',
-          cancelButtonText:'排行'
+          confirmButtonText: '邀请',
+          cancelButtonText: '排行'
         });
       },
       /*
       * 游戏开始
-      * 倒计时结束后重置倒计时，开始下一个单词
       * 获取对应的图片
       * 洞中出现字母
-      * 是个字母都出现完时结束游戏
       * */
-      begin(){
-        if(num>9){this.gameOver();return false;}
-        this.time=20;
-        this.getImg(thisGameWords[num]);
-        this.giveWord(thisGameWords[num]);
-        num++;
+      begin() {
+        clearInterval(giveLetterTimer)
+        this.getTrueWord();
+        this.getImg(trueWord);
+        this.giveLetter(trueWord);
+        !hideHamsterTime && (this.hideHamster());
       },
-      /*
-      * 计算正确的单词每个字母出现的时间
-      * */
-      // trueWordsTime(word){
-      //   //计算每个单词出现的平均时间段
-      //   let averageTime = Math.floor(this.time / word.length);
-      //   let times = []
-      //   for (let i = 0; i < word.length; i++) {
-      //     let n,m;
-      //     //获取每个单词对应出现的时间段的随机时间
-      //     //设置一些边界值，避免正确的字母出现在第一个（还没准备好），最后一个（已经来不及）
-      //     n=i===0?1:averageTime * i;
-      //     m=i===word.length-1?averageTime * (i+1)-2:averageTime * (i+1);
-      //     let timeSection = Math.floor(random(n,m))
-      //     times.push({word: word[i], time: timeSection})
-      //   }
-      //   return times;
-      // },
-      // giveWord(word) {
-      //   console.log(word)
-      //   let times = this.trueWordsTime(word);
-      //   let n = 0;
-      //   console.log(times, 'times is :')
-      //   let timer = setInterval(() => {
-      //     this.time--
-      //     n++;
-      //     let isTrueWord = false;
-      //     /*
-      //     * 如果此时是正确字母应该出现的时间，把正确字母随机地放到洞中
-      //     * */
-      //     times.forEach((item) => {
-      //       if (n === item.time) {
-      //         let index = Math.floor(Math.random() * 9);
-      //         this.words.splice(index, 1, item.word)
-      //         isTrueWord = true;
-      //         this.showHamster(index,'block')
-      //       }
-      //     })
-      //     //如果没有出正确的单词，就在随机的洞中出现一个随机的单词
-      //     if (!isTrueWord) {
-      //       let index = Math.floor(Math.random() * 9);
-      //       let wordIndex = Math.floor(Math.random() * 25);
-      //       this.words.splice(index, 1, initWords[wordIndex])
-      //       this.showHamster(index,'block')
-      //     }
-      //     //倒计时结束时，清除定时器，开始下一个单词
-      //     if (this.time <= 0) {
-      //       clearInterval(timer)
-      //       //alert('fail')
-      //       this.begin();
-      //     }
-      //   }, 1000)
-      // },
-      giveWord(word){  //stars
-
+      hideHamster(){
+        hideHamsterTime=setInterval(()=>{
+          this.letters.forEach((item,index)=>{
+            if(item.letter&&(Date.now()-item.showTime>2000)){
+              this.showHamster(index,'none')
+            }
+          })
+        },2000)
       },
-      showHamster(index,style){
-        //console.log(this.$refs['hole'][index].style.top,'hole is :========')
-        this.$refs['hamster'][index].style.display=style;
+      getTrueWord() {
+        trueWord = allWords[Math.floor(random(0, wordLength))]
+      },
+      /*下一个单词*/
+      nextWord() {
+        this.begin()
+      },
+      giveLetter(word) {
+        setTimeout(() => {
+          giveLetterTimer = setInterval(() => {
+            let index = Math.floor(random(0, 9))
+            let letterIndex = Math.floor(random(0, word.length))
+            let letter = word[letterIndex]
+            this.letters.splice(index, 1, {letter:letter,showTime:Date.now()})
+            this.showHamster(index, 'block')
+            this.time-=1000
+            if (this.time <= 0) {
+              clearInterval(giveLetterTimer)
+              this.gameOver()
+            }
+          }, 1000)
+        }, 1000)
+      },
+      showHamster(index, style) {
+        this.$refs['hamster'][index].style.display = style;
       },
       /*
       * 选择正确时，得分，开始下一个单词
       * */
-      choseWord(index, word) {
-        this.showHamster(index,'none')
-        this.words.splice(index, 1, '')
-        selectWord += word;
-        if (selectWord === thisGameWords[num-1]) {
-          this.score++;
-          //alert('Congratulations')
-          this.begin();
-          selectWord =''
+      choseWord(index, letter) {
+        this.showHamster(index, 'none')
+        this.letters.splice(index,1,{letter:'',showTime:0})
+        selectWord += letter
+        if (selectWord === trueWord) {
+          this.score++
+          selectWord = ''
+          this.good = true
+          let time = setTimeout(() => {
+            this.good = false
+            clearTimeout(time)
+            this.begin()
+          }, 1000)
         }
       }
     }
