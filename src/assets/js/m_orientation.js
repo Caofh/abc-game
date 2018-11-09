8708f297ceaa58ac0184154f9af730a4e3b55380
caofanghui <caofanghui@vipkid.com.cn>
2018-11-09 08:32:02 +0800

    fb

--- src/assets/js/m_orientation.js
+++ src/assets/js/m_orientation.js
@@ -1,19 +1,19 @@
 
 (function() {
-    var html = document.documentElement;
-    var setFontSize = function() {
-      let width = oriChange() // 获取当前屏幕宽度，适配横屏游戏.
+  var html = document.documentElement;
+  var setFontSize = function() {
+    let width = oriChange() // 获取当前屏幕宽度，适配横屏游戏.
 
-        html.style.fontSize = width / 10 + 'px';
-        width = null;
-    };
+    html.style.fontSize = width / 10 + 'px';
+    width = null;
+  };
+  setFontSize();
+  window.addEventListener('resize',function(){
     setFontSize();
-    window.addEventListener('resize',function(){
-        setFontSize();
-    })
-    window.addEventListener('orientationchange', function() {
-        setFontSize();
-    });
+  })
+  window.addEventListener('orientationchange', function() {
+    setFontSize();
+  });
 })();
 
 // 横竖屏幕切换，适配横屏游戏.
@@ -24,10 +24,10 @@ function oriChange () {
   let width = ''
 
   if ( now_width >= now_height ) { // 横屏
-    width = now_width || document.getBoundingClientRect().width;
+    width = now_width ? now_width : document.getBoundingClientRect().width;
   } else {
-    width = now_height || document.getBoundingClientRect().height;
-
-    return width
+    width = now_height ? now_height : document.getBoundingClientRect().height;
   }
+
+  return width
 }
